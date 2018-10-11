fa84beefee8ec9fde23890226b043d0340aafab6
jarhuba <jarhuba@gmail.com>
2018-10-11 09:02:51 +0200

    First commit, complete version

---
+++ src/main/java/com/kodilla/kodillam1p1z/KodillaM1p1zApplication.java
@@ -0,0 +1,12 @@
+package com.kodilla.kodillam1p1z;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaM1p1zApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaM1p1zApplication.class, args);
+    }
+}
